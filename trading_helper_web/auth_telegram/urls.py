d342526957a68a4947b8413b5fbf4c5e26c90c1d
ulug-sodikov <dev03@personhunters.com>
2025-01-30 13:32:20 +0500

    Create logic of tg oauth data validity checking.

--- trading_helper_web/auth_telegram/urls.py
+++ trading_helper_web/auth_telegram/urls.py
@@ -6,4 +6,5 @@ from . import views
 app_name = 'auth_telegram'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login/', views.login, name='login')
 ]
--- trading_helper_web/auth_telegram/views.py
+++ trading_helper_web/auth_telegram/views.py
@@ -1,7 +1,9 @@
+import hmac
 from os import getenv
+from hashlib import sha256
 
 from dotenv import load_dotenv
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse, HttpResponseBadRequest
 from django.template import loader
 
 
@@ -13,3 +15,23 @@ def index(request):
     context = {'tg_bot_username': getenv('TG_BOT_USERNAME')}
 
     return HttpResponse(template.render(context, request))
+
+
+def login(request):
+    tg_oauth_data = request.POST.dict()
+    try:
+        tg_oauth_data.pop('csrfmiddlewaretoken')
+    except KeyError:
+        return HttpResponseBadRequest()
+
+    data_check_string = '\n'.join(
+        f'{k}={v}' for k, v in sorted(tg_oauth_data.items())
+        if k != 'hash'
+    )
+    tg_bot_token = getenv('TG_BOT_TOKEN')
+    secret_key = sha256(tg_bot_token.encode()).digest()
+    generated_hash = hmac.new(
+        secret_key, data_check_string.encode(), sha256
+    ).hexdigest()
+
+    return HttpResponse(f'{generated_hash == tg_oauth_data.get("hash")}')
