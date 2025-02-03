a0b025d6490ac48d326905527896656f204a22a6
ulug-sodikov <dev03@personhunters.com>
2025-02-03 10:54:25 +0500

    Add shebang and todo comment for autostart script.

--- metatrader5_api/scripts/autostart_script.sh
+++ metatrader5_api/scripts/autostart_script.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # This script runs on the background silently and takes some time to complete.
 # You will not see the result of this script instantly.
 
@@ -6,3 +8,5 @@ wine /mt5setup.exe
 
 # Install python windows version silently.
 wine /python_installer.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
+
+# TODO: check if mt5 and python age installed and if yes don't install it again
