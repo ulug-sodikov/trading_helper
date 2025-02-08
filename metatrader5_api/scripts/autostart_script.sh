3bd9428c4dc6ed6a36425d6258612e251b3483fc
ulug-sodikov <dev03@personhunters.com>
2025-02-08 12:44:50 +0500

    Install metatrader and python for windows only if they are not installed yet.

--- metatrader5_api/scripts/autostart_script.sh
+++ metatrader5_api/scripts/autostart_script.sh
@@ -3,10 +3,12 @@
 # This script runs on the background silently and takes some time to complete.
 # You will not see the result of this script instantly.
 
-# Launch metatrader5 installer.
-wine /mt5setup.exe
+if [ ! -f "/config/.wine/drive_c/Program Files/MetaTrader 5/terminal64.exe" ]; then
+    # Launch metatrader5 installer.
+    wine /mt5setup.exe
+fi
 
-# Install python windows version silently.
-wine /python_installer.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
-
-# TODO: check if mt5 and python age installed and if yes don't install it again
+if [ ! -f "/config/.wine/drive_c/Program Files/Python39/python.exe" ]; then
+    # Install python windows version silently.
+    wine /python_installer.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
+fi
