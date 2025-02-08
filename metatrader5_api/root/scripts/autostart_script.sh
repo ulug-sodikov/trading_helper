72ec0d4eb6e859c404e32cee43f8c411a1e0b251
ulug-sodikov <dev03@personhunters.com>
2025-02-08 14:15:15 +0500

    Install required python libraries.

--- metatrader5_api/root/scripts/autostart_script.sh
+++ metatrader5_api/root/scripts/autostart_script.sh
@@ -11,4 +11,5 @@ fi
 if [ ! -f "/config/.wine/drive_c/Program Files/Python39/python.exe" ]; then
     # Install python windows version silently.
     wine /python_installer.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
+    wine pip install --no-cache-dir -r /server/requirements.txt
 fi
