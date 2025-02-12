737c0732a44f5da3bc489e6dd9cf7ba328eb0768
ulug-sodikov <dev03@personhunters.com>
2025-02-12 13:53:59 +0500

    Create Notification model.

--- trading_helper_web/price_notifications/models.py
+++ trading_helper_web/price_notifications/models.py
@@ -1,3 +1,60 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Notification(models.Model):
+    class PriceType(models.TextChoices):
+        BID_PRICE = "BID_PRICE",
+        ASK_PRICE = "ASK_PRICE"
+
+    class ComparisonType(models.TextChoices):
+        GREATER_THAN = "GREATER_THAN"
+        LESS_THAN = "LESS_THAN"
+
+    symbol = models.CharField(max_length=20)
+    target_price = models.DecimalField(max_digits=50, decimal_places=30)
+    comparison_type = models.CharField(max_length=12, choices=ComparisonType)
+    tracking_price_type = models.CharField(max_length=9, choices=PriceType)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    @classmethod
+    def get_triggered_notifications(cls, symbol, bid, ask):
+        """
+        Returns a list of triggered notifications for the given symbol
+        and ticks.
+
+        Four possible scenarios can be specified for notification
+        to trigger:
+        - If bid price is greater than target price, and tracking price is bid price.
+        - If bid price is less than target price, and tracking price is bid price.
+        - If ask price is greater than target price, and tracking price is ask price.
+        - If ask price is less than target, and tracking price is ask price.
+        """
+        query = models.Q(
+            tracking_price_type=cls.PriceType.BID_PRICE,
+            comparison_type=cls.ComparisonType.GREATER_THAN,
+            target_price__lt=bid
+        )
+        query |= models.Q(
+            tracking_price_type=cls.PriceType.BID_PRICE,
+            comparison_type=cls.ComparisonType.LESS_THAN,
+            target_price__gt=bid
+        )
+        query |= models.Q(
+            tracking_price_type=cls.PriceType.ASK_PRICE,
+            comparison_type=cls.ComparisonType.GREATER_THAN,
+            target_price__lt=ask
+        )
+        query |= models.Q(
+            tracking_price_type=cls.PriceType.ASK_PRICE,
+            comparison_type=cls.ComparisonType.LESS_THAN,
+            target_price__gt=ask
+        )
+
+        return cls.objects.filter(query, symbol=symbol)
+
+    def __str__(self):
+        return (
+            f'IF {self.symbol} {self.tracking_price_type} {self.comparison_type} '
+            f'{self.target_price}, NOTIFY {self.user.username}'
+        )
