d27069225f3e06adb8a61cc65f8241056adec25c
ulug-sodikov <dev03@personhunters.com>
2025-02-14 14:57:14 +0500

    Build frontend of price_notification app.

---
+++ trading_helper_web/price_notifications/static/price_notifications/script.js
@@ -0,0 +1,24 @@
+const WS_URL = document.currentScript.getAttribute('data-ws-url');
+
+const connectToWSServer = (onMessage) => {
+    const socket = new WebSocket(WS_URL);
+    // If connection is failed reestablish connection after 2 seconds.
+    socket.addEventListener("close", () => {
+        setTimeout(connectToWSServer, 2000, onMessage);
+    });
+    socket.addEventListener("message", onMessage);
+};
+
+const onWsMessage = (event) => {
+    const tick = JSON.parse(event.data);
+    if (tick.symbol === 'XAUUSD') {
+        // This spread calculation is only valid for XAUUSD
+        const xauusdSpread = tick.ask*100 - tick.bid*100;
+        const priceDiv = document.getElementById('instrument-price-number');
+        const spreadDiv = document.getElementById('instrument-spread-info');
+        priceDiv.textContent = `${tick.bid}`;
+        spreadDiv.textContent = `SPREAD ${xauusdSpread} PIPS`;
+    }
+};
+
+connectToWSServer(onWsMessage);
