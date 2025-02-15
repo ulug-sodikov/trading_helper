56818468c710ea7cf6a1380cd8682231a4bbf72f
ulug-sodikov <dev03@personhunters.com>
2025-02-15 14:55:00 +0500

    Change comment.

--- trading_helper_web/price_notifications/adjust_mt5_buffer.py
+++ trading_helper_web/price_notifications/adjust_mt5_buffer.py
@@ -13,7 +13,8 @@ def adjust_mt5_buffer():
 
     # Symbols that are not present in database but present in mt5 symbols_buffer
     ghost_symbols = mt5_symbols_buffer.difference(symbols)
-    # XAUUSD ticks are used in frontend.
+    # XAUUSD ticks are used in frontend, thus XAUUSD shoud not be removed
+    # from mt5 symbols_buffer
     ghost_symbols.discard('XAUUSD')
     for symbol in ghost_symbols:
         requests.delete(f'http://metatrader5_api_service:8080/symbols_buffer/{symbol}')
