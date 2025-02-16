4c21127e8b40cf8e54b06be4c1966928dd6d42ec
ulug-sodikov <dev03@personhunters.com>
2025-02-16 20:49:46 +0500

    Add code that runs at django app start.

--- trading_helper_web/price_notifications/apps.py
+++ trading_helper_web/price_notifications/apps.py
@@ -1,6 +1,13 @@
+import requests
 from django.apps import AppConfig
 
 
 class PriceNotificationsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "price_notifications"
+
+    def ready(self):
+        # XAUUSD real-time price is displayed in '/price_notification/' page.
+        requests.patch(
+            'http://metatrader5_api_service:8080/symbol_exists/XAUUSD'
+        )
