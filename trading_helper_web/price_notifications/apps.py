ac7aba551a75f27291cbaeca37f74499db9ced83
ulug-sodikov <dev03@personhunters.com>
2025-02-17 11:25:35 +0500

    Create polling engine.

--- trading_helper_web/price_notifications/apps.py
+++ trading_helper_web/price_notifications/apps.py
@@ -1,5 +1,9 @@
+import threading
+
 import requests
 from django.apps import AppConfig
+
+from .price_polling_loop import start_price_polling_loop
 
 
 class PriceNotificationsConfig(AppConfig):
@@ -11,3 +15,15 @@ class PriceNotificationsConfig(AppConfig):
         requests.patch(
             'http://metatrader5_api_service:8080/symbol_exists/XAUUSD'
         )
+
+        # Since PriceNotificationsConfig.ready method might be executed twice,
+        # price-polling-loop thread existence should be checked before creating
+        # it, otherwise two threads can be created
+        for _thread in threading.enumerate():
+            if _thread.name == 'price-polling-loop':
+                break
+        else:
+            new_thread = threading.Thread(
+                target=start_price_polling_loop, name='price-polling-loop'
+            )
+            new_thread.start()
---
+++ trading_helper_web/price_notifications/utils/immortal_ws.py
@@ -0,0 +1,35 @@
+import time
+
+import websocket
+
+
+class ImmortalWebSocket(websocket.WebSocket):
+    """
+    WebSocket that will try to reestablish a connection every two seconds
+    if the connection has been closed by the websocket server.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # https://peps.python.org/pep-0412/
+        self._url = None
+
+    def connect(self, url, **options):
+        self._url = url
+
+        while True:
+            try:
+                return super().connect(url, **options)
+            except (
+                    ConnectionRefusedError,
+                    ConnectionResetError,
+                    websocket.WebSocketConnectionClosedException
+            ):
+                print("reconnection")
+                time.sleep(2)
+
+    def recv(self):
+        try:
+            return super().recv()
+        except websocket.WebSocketConnectionClosedException:
+            self.connect(self._url)
