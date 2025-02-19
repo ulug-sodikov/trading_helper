e140fe36d7dc5c5b96ef7aa7280148056fe6f741
ulug-sodikov <dev03@personhunters.com>
2025-02-19 14:09:47 +0500

    Adjust code style.

--- trading_helper_web/price_notifications/adjust_mt5_buffer.py
+++ trading_helper_web/price_notifications/adjust_mt5_buffer.py
@@ -5,11 +5,15 @@ from .models import Notification
 
 def adjust_mt5_buffer():
     symbols = {row['symbol'] for row in Notification.objects.values('symbol')}
-    response = requests.get('http://metatrader5_api_service:8080/symbols_buffer')
+    response = requests.get(
+        'http://metatrader5_api_service:8080/symbols_buffer'
+    )
     mt5_symbols_buffer = set(response.json()['symbols_buffer'])
 
     for symbol in symbols.difference(mt5_symbols_buffer):
-        requests.patch(f'http://metatrader5_api_service:8080/symbols_buffer/{symbol}')
+        requests.patch(
+            f'http://metatrader5_api_service:8080/symbols_buffer/{symbol}'
+        )
 
     # Symbols that are not present in database but present in mt5 symbols_buffer
     ghost_symbols = mt5_symbols_buffer.difference(symbols)
@@ -17,4 +21,6 @@ def adjust_mt5_buffer():
     # from mt5 symbols_buffer
     ghost_symbols.discard('XAUUSD')
     for symbol in ghost_symbols:
-        requests.delete(f'http://metatrader5_api_service:8080/symbols_buffer/{symbol}')
+        requests.delete(
+            f'http://metatrader5_api_service:8080/symbols_buffer/{symbol}'
+        )
